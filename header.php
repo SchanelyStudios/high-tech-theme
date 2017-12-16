766371fafa6b351b561c92dbc011f66cf0b326e9
z1bowen1 <z1bowen1@yahoo.com>
2017-12-16 12:21:28 -0500

    Initial file set from previous repo

---
+++ header.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+    <head>
+    <title><?php wp_title(); ?></title>
+    <meta charset="<?php bloginfo( 'charset' ); ?>" />
+    <link rel="profile" href="http://gmpg.org/xfn/11" />
+    <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link type="text/css" rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" />
+    <?php wp_head(); ?>
+    </head>
+    <body>
+      <header class="row">
+          <div class="row">
+              <img src="pics/logo-shadow-08.png">
+              <p>Aenean at lorem magna. Pellentesque in lacus ut eros semper tempor.</p>
+          </div>
+      </header>
+    <main>
