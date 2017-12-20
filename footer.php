a8c49a516b8caddc7afa3116b3a6bd1d4d1254e4
z1bowen1 <z1bowen1@yahoo.com>
2017-12-20 11:15:58 -0500

    updates

--- footer.php
+++ footer.php
@@ -1,6 +1,9 @@
 </main>
 <footer>
   <!-- Footer content here -->
+  <img src="pics/logo-white.png" width="100px" height="100px">
+  <h2>thankyou!</h2>
+  <p>Copyright ©2016 by Zac Bowen. All rights reserved.
 </footer>
 </body>
 </html>
